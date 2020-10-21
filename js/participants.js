2a4815657c415e6b4c4f004ff2d9d694c11222e3
Ansh Dawda <ansh.dawda@gmail.com>
2020-10-21 20:22:59 +0530

    add Ansh Dawda to participants list

--- js/participants.js
+++ js/participants.js
@@ -8,11 +8,11 @@ const participants = [
     website: "https://pawankolhe.com",
   },
   {
-    name: "Saurabh Shukla",
+    name: "Ansh Dawda",
     college: "Amity University Mumbai",
     city: "Navi Mumbai",
-    github: "Fangston0",
-    linkedin: "https://www.linkedin.com/in/saurabh-shukla-680425117/",
+    github: "InsaanTheHuman",
+    linkedin: "",
     website: "",
   },
 ];
